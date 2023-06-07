41121560241babae281184f7d75b612becb1eaa8
Cedric Clyburn <cedricclyburn@gmail.com>
2023-06-06 23:22:39 -0600

    Added supplementary pages for RPS frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Capture from './Capture'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from './Homepage';
+import InstructionsPage from "./Instructions";
+import Capture from "./Capture";
+import Results from "./Results";
 
 function App() {
   return (
-    <>
-      <h1>RPS</h1>
-      <Capture></Capture>
-    </>
-  )
+    <Router>
+      <h1>Rock, Paper, Scissors!</h1>
+      <Routes>
+        <Route path="/instructions" element={<InstructionsPage />} />
+        <Route path="/capture" element={<Capture />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
